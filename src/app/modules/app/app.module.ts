232db4a960f6a26b010e15d7bc35ec8030fd2526
FelipeDasr <felipe.heiker07@gmail.com>
2024-06-09 18:49:02 -0400

    feat: serve static files

--- src/app/modules/app/app.module.ts
+++ src/app/modules/app/app.module.ts
@@ -1,13 +1,25 @@
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { Module } from '@nestjs/common';
 
 import { AmqplibModule } from '../amqp/amqp.module';
 import { HttpModule } from '../http/http.module';
 
 import { ConvertCsvToJsonUsecase } from 'src/app/useCases/conversions/convertCSVToJSON.usecase';
+import { WebSocketsGateway } from 'src/core/gateways/websocket.gateway';
 import { AppService } from './app.service';
 
+import * as path from 'path';
+
 @Module({
-  imports: [HttpModule, AmqplibModule],
-  providers: [AppService, ConvertCsvToJsonUsecase],
+  imports: [
+    HttpModule,
+    AmqplibModule,
+    // Frontend
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, '..', '..', '..', 'static', 'public'),
+      renderPath: '/',
+    }),
+  ],
+  providers: [AppService, WebSocketsGateway, ConvertCsvToJsonUsecase],
 })
 export class AppModule {}
